b4ce75dc6be4ee651e2cdf87aa56ad9808488f38
souhirdaadouch <Souhir.Daadouch@insat.u-carthage.tn>
2020-04-04 13:01:49 +0200

    final

--- src/app/form/demande-affiliation/demande-affiliation.component.ts
+++ src/app/form/demande-affiliation/demande-affiliation.component.ts
@@ -28,43 +28,10 @@ export class DemandeAffiliationComponent implements OnInit {
       pres_comitedirecteur: new FormControl(null, Validators.required),
       secretaire_comitedirecteur: new FormControl(null, Validators.required),
       tresorier_comitedirecteur: new FormControl(null, Validators.required),
-      membres_comitedirecteur: new FormGroup({
-        name: new FormControl(null, Validators.required),
-        dateNaissance: new FormControl(null, Validators.required),
-        cin: new FormControl(null, Validators.required),
-        tache: new FormControl(null, Validators.required),
-        mobile: new FormControl(null, Validators.required),
-        mobile2: new FormControl(null, Validators.required),
-        email: new FormControl(null, Validators.required),
-        adresse: new FormControl(null, Validators.required),
-        image: new FormControl(null, Validators.required)
-      }),
       pres_sectionathlet: new FormControl(null, Validators.required),
       secretaire_sectionathlet: new FormControl(null, Validators.required),
       tresorier_sectionathlet: new FormControl(null, Validators.required),
       directeur_tech: new FormControl(null, Validators.required),
-      membres_sectionathlet: new FormGroup({
-        name: new FormControl(null, Validators.required),
-        dateNaissance: new FormControl(null, Validators.required),
-        cin: new FormControl(null, Validators.required),
-        tache: new FormControl(null, Validators.required),
-        mobile: new FormControl(null, Validators.required),
-        mobile2: new FormControl(null, Validators.required),
-        email: new FormControl(null, Validators.required),
-        adresse: new FormControl(null, Validators.required),
-        image: new FormControl(null, Validators.required)
-      }),
-      entraineurs: new FormGroup({
-        name: new FormControl(null, Validators.required),
-        dateNaissance: new FormControl(null, Validators.required),
-        cin: new FormControl(null, Validators.required),
-        tache: new FormControl(null, Validators.required),
-        mobile: new FormControl(null, Validators.required),
-        mobile2: new FormControl(null, Validators.required),
-        email: new FormControl(null, Validators.required),
-        adresse: new FormControl(null, Validators.required),
-        image: new FormControl(null, Validators.required)
-      }),
       nom_perschargee: new FormControl(null, Validators.required),
       adresse_perschargee: new FormControl(null, Validators.required)
     });
